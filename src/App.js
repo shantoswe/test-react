d562203d7c1450762e9c435dacfa50380d055a7f
shantoswe <104422097+shantoswe@users.noreply.github.com>
2023-02-19 12:06:11 +0600

    with layout component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Layout from './components/Layout';
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
       <Header logo={logo}/>
+      <Layout/>
     </div>
   );
 }
---
+++ src/components/Layout.js
@@ -0,0 +1,12 @@
+export default function Layout() {
+
+    return(
+        <main>
+            <p className="body_text">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus sint aliquam molestiae nobis vitae deserunt explicabo doloribus ipsa veniam ut.
+                
+            </p>
+        </main>
+    )
+    
+}
